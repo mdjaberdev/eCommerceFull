98acac041ae24e2612ccd70f1e45301f64be9d53
mdjaberdev <mdjaber.dev@gmail.com>
2026-09-22 02:05:26 +0600

    verifyControllerDone

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,19 +1,12 @@
 const express = require("express");
-const {
-  registrationController,
-  loginController,
-  verificationemailController,
-  forgotPasswordController,
-  resetPasswordController,
-} = require("../controllers/authControllers");
-
+const { registrationController, verificationEmailController } = require("../controllers/authControllers");
 
 const router = express.Router();
 
 router.post("/registration", registrationController);
-router.post("/login", loginController);
-router.post("/verify/:token", verificationemailController);
-router.post("/forgetPassword", forgotPasswordController);
-router.post("/resetpassword/:token", resetPasswordController);
+router.post("/verify/:token", verificationEmailController);
+
+
+
 
 module.exports = router;
--- backend/utils/emailSender.js
+++ backend/utils/emailSender.js
@@ -2,32 +2,29 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   port: 587,
-  secure: false, 
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
 
+const verificationEmail = async (email, verificationToken) => {
+  const info = await transporter.sendMail({
+    from: '"Example Team" <mdjaber.dev@gmail.com>', // sender address
+    to: email, // list of recipients
+    subject: "Verification Email", // subject line
+    html: `<a href="http://localhost:5000/api/v1/auth/verify/${verificationToken}">Click here to verify your account</a>`, // HTML body
+  });
+};
 
-const verificationEmail = async (email, token)=>{
-     const info = await transporter.sendMail({
-       from: '"Example Team" <mdjaber.dev@gmail.com>', // sender address
-       to: email, // list of recipients
-       subject: "Verification Email", // subject line
-       html: `This is your token ${token}`, // HTML body
-     });
-  return info
-}
+const forgetPasswordEmail = async (email, resetPasswordToken) => {
+  const info = await transporter.sendMail({
+    from: '"Example Team" <mdjaber.dev@gmail.com>', // sender address
+    to: email, // list of recipients
+    subject: "Reset Password", // subject line
+    html: `<a href="http://localhost:5000/api/v1/auth/resetPassword/${resetPasswordToken}">Click here </a>`, // HTML body
+  });
+};
 
-const forgetPassEmail = async (email, token)=>{
-   const info = await transporter.sendMail({
-       from: '"Example Team" <mdjaber.dev@gmail.com>', // sender address
-       to: email, // list of recipients
-       subject: "Forget Password", // subject line
-       html: `This is your token ${token}`, // HTML body
-     });
-  return info
-}
-
-module.exports = { verificationEmail, forgetPassEmail };
+module.exports = { verificationEmail, forgetPasswordEmail };
