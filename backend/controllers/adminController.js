83e68114d9e6d2f18862c69d27cacfd6e4910b58
mdjaberdev <mdjaber.dev@gmail.com>
2026-09-22 22:42:15 +0600

    admin

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,28 @@
+const User = require("../models/userSchema");
+const adminController = async (req, res) => {
+  res.send("Hello admin");
+};
+
+const allUserController = async (req, res) => {
+  const users = await User.find({}).select("-password");
+  res.status(200).json({
+    success: true,
+    message: `${users.length} users found`,
+    data: users,
+  });
+};
+
+const deleteUserController = async (req, res) => {
+  const { id } = req.params;
+  const deleteUser = await User.findByIdAndDelete(id);
+  if (!deleteUser) {
+    return res.status(400).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  res.stat
+};
+
+module.exports = { adminController, allUserController, deleteUserController };
--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,21 @@ require("dotenv").config();
 const express = require("express");
 const dbConnection = require("./config/dbConnection");
 const router = require("./routes/authRouter");
+const userRouter = require("./routes/userRouter");
+const adminRouter = require("./routes/adminRouter");
+const vendorRouter = require("./routes/vendorRouter");
+const { userMiddleware, adminMiddleware, vendorMiddleware } = require("./middlewares/roleMiddleware");
 const app = express();
-dbConnection()
+dbConnection();
 
 app.use(express.json());
 
-app.use("/api/v1/auth", router)
+app.use("/api/v1/auth", router);
+app.use("/api/v1/user", userMiddleware, userRouter);
+app.use("/api/v1/admin",adminMiddleware, adminRouter);
+app.use("/api/v1/vendor", vendorMiddleware, vendorRouter);
 
-const port = process.env.DB_PORT || 8000
+const port = process.env.DB_PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Server is running ${port}`);
